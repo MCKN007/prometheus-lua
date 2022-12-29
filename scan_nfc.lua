cb1581a65de86fbd1a101f71a5d31b5089e8c2f6
MCBBS_007 <88782291+MCKN007@users.noreply.github.com>
2022-12-29 11:20:48 +0800

    更新了扫描方法
    在运行的时候会先去检测有没有日志文件，如果有的话就直接扫描，没有的话则提示安装驱动

--- scan_nfc.lua
+++ scan_nfc.lua
@@ -1,12 +1,27 @@
 print("开始运行扫描程序")
-local scan_nfc = os.execute("dpkg -s libnfc-dev")
-if scan_nfc == 0 then --判断是否安装，返回值为0代表安装了
-    print("安装成功")
-    print("请检查设备是否连接，如果能看到这段话停留那么就是已经连接好了")
-    os.execute("nfc-poll")--执行扫描程序
-    else--如果没有安装那么
-        print("请返回步骤1安装驱动的")
-        os.execute("qterminal -e lua prometheus_main.lua")
-        os.execute("exit")--弹出终端窗口返回步骤1去执行
+
+local lfs = require("lfs")
+local ChenMengyuan = "prometheus.log"
+local ERROR = "error.log"
+local attr = lfs.attributes(ChenMengyuan)
+if attr then
+  -- 文件存在
+  local scan_nfc = os.execute("dpkg -s libnfc-dev")
+else
+  -- 文件不存在
+  print("No")
+ 
+  -- 创建新文件
+  local file = io.open(ERROR, "w")
+  if file then
+    -- 创建成功
+    print("请检查驱动程序")
+    os.execute("cat > error.log << 驱动没有安装或者没有配置设备")
+    file:close()
+  else
+    -- 创建失败
+    print("权限不足！！！")
+    end
 end
+os.execute("qterminal -e lua prometheus_main.lua")
 os.exit()
