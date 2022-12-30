dbcb470d3d9e2d3781360c037c817d0c654925b1
MCBBS_007 <88782291+MCKN007@users.noreply.github.com>
2022-12-30 20:45:31 +0800

    可以被主程序调用
    运行的时候可以根据输入的文件名去读取文件然后保存

--- readcard_nfc.lua
+++ readcard_nfc.lua
@@ -4,5 +4,5 @@ local savefile_name = io.read()--读取输入的文件名
 print("开始读取数据")
 print("文件读取完成将保存在当前文件夹下面")
 os.execute("mfoc -O" .. savefile_name .. ".dump")
-
-
+os.exit()
+os.execute("lua prometheus_main.lua")
