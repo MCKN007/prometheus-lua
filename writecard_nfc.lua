9d4b004174d8374852d12bd652d6baf5a44326a2
MCBBS_007 <88782291+MCKN007@users.noreply.github.com>
2023-01-03 15:00:17 +0800

    修复了写入时找不到密钥的问题

--- writecard_nfc.lua
+++ writecard_nfc.lua
@@ -3,7 +3,7 @@ io.write("请将要写入的文件拖这里然后回车确认：")
 local write1 = io.read()--读取输入的文件名
 io.write("请将密钥文件拖到这里然后回车确认：")
 local write2 = io.read()--同上
-os.execute("nfc-mfclassic W "..write2.. ".dump"..write1..".dump")
+os.execute("nfc-mfclassic W a"..write2.. ".dump"..write1..".dump")
 print("卡片写入成功")
 os.exit()
 os.execute("lua prometheus_main.lua")
